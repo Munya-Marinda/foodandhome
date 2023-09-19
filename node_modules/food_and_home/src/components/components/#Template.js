35503300aab811197297a89d3763fcdf99ee82b6
Munya M <84540577+Munya-Marinda@users.noreply.github.com>
2023-09-19 11:28:04 +0200

    Delete node_modules directory

--- node_modules/food_and_home/src/components/components/#Template.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { connect } from "frontity";
-import Link from "@frontity/components/link";
-
-const Template = ({ state }) => {
-  return <></>;
-};
-
-export default connect(Template);
